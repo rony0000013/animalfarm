7e6cbac769efc2a37e9fffbc374620ec8d0320f3
Rounak Sen <103501472+rony0000013@users.noreply.github.com>
2023-05-19 00:15:37 +0530

    final

---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import Chance from "chance";
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const chance = new Chance();
+
+const animals = [...Array(250).keys()].map((id) => ({
+        id,
+        name: chance.name(),
+        age: chance.age(),
+        type: chance.animal()
+}));
+
+
+app.get("/", (req, res) => {
+
+    const q =  req.query.q?.toLowerCase() || "";
+    const results = animals.filter(animal => animal.type.toLowerCase().includes(q));
+
+    res.send(results);
+}); 
+
+app.listen(8080, () => {
+    console.log("listening on port http://localhost:8080");
+})
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import { TextField, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import { List, ListItem, ListItemText, Divider } from '@mui/material';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [animals, setAnimals] = useState([]);
+
+useEffect(() => {
+  const lastQuery = localStorage.getItem('lastQuery');
+  search(lastQuery);
+}, []);
+
+
+/**
+ * Asynchronously searches for animals data using the given query string. 
+ *
+ * @param {string} q - The query string to use for searching for animal data.
+ * @return {Promise<void>} - A promise that resolves when the search is complete and animal data has been set.
+ */
+  const search = async (q) => {
+    const response = await fetch('http://localhost:8080?' + new URLSearchParams({ q }));
+    const data = await response.json();
+    setAnimals(data);
+
+    localStorage.setItem('lastQuery', q);
+  }
 
   return (
     <>
-      
+    <Box component="div" height={"100vh"} p={4} m={2} alignItems="center" justifyContent="start" display="flex" flexDirection="column" bgcolor="background.paper">
+      {/* <h1 className='text-2xl'></h1> */}
+      <Typography variant="h1" m={3}>🐐 Animal Farm</Typography>
+
+      <TextField id="filled-basic" label="Search" variant="filled" sx={{"margin-bottom": "1.5rem", margin: "1rem"}} onChange={e => search(e.target.value)} style={{ width: '700px' }} />
+
+      <List  sx={{ width: '100%', maxWidth: 500, bgcolor: 'background.paper' }} >
+        {animals.map(animal => (
+            <>
+            <ListItem>
+            <ListItemText
+              primary={animal.type}
+              secondary={`${animal.name} - ${animal.age} years old`}
+              />
+            </ListItem>
+            <Divider/>
+            </>
+          ))
+        }
+        {animals.length === 0 && "No animals"}
+      </List>
+    </Box>
     </>
   )
 }
 
-export default App
+export default App;
